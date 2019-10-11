a62dff8231a9d67d6c2b5daabc6191abb71478b4
Wali Rahman <wali.rahman89@yahoo.com>
2019-10-11 09:24:51 -0400

    Update picking-numbers.js
    Added better solution

--- picking-numbers.js
+++ picking-numbers.js
@@ -1,13 +1,18 @@
 function pickingNumbers(a) {
-    a.sort((a,b) => a-b)
-    var ans = [];
-    for(var i=0; i < a.length; i++) {
-        if((Math.abs(a[i]-a[i+1])) <= 1) {
-            ans.push(a[i]);
-        };
-
+ a = a.map(Number);
+    var res = 0;
+    for(var i=0;i<100;i++){
+        var numberA = i;
+        var numberB = i+1;
+        var curRes = 0;
+        for(var j=0;j<a.length;j++){
+            if(a[j] === numberA || a[j] === numberB){
+                curRes++;
+            }
+        }
+        if(curRes > res){
+            res = curRes;
+        }
     }
-
-    return ans.length;
-
+    return res;
 }
